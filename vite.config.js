d7f61aa454b4d70c4519ce9ce80c862b1ebe3ef5
fysay <maprogodis@gmail.com>
2023-12-03 15:37:27 -0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mscode/",
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  base: "/fysay.github.io/"
+  assetsInclude: ['**/*.glb']
 })
