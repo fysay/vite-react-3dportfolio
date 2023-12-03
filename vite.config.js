23cf2e10b1794932f1f71bd132cfd302924d5849
fysay <maprogodis@gmail.com>
2023-12-03 16:02:53 -0500

    add: initial files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fysay.github.io/",
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  base: "/vite-react-3dportfolio/"
 })
